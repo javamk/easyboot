133f225dd18554c951eb08728df009acfdb24e94
mayunjing <mayunjing@DESKTOP-O3E85LL>
2020-11-11 14:23:19 +0800

    测试

---
+++ src/test/java/com/mayunjing/easyboot/EasybootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayunjing.easyboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
